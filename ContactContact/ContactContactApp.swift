0f1fa914efaff4ff69410b94cc162932bbab178b
Richard Adem <richy486@gmail.com>
2024-06-19 14:14:13 +0200

    Initial Commit

---
+++ ContactContact/ContactContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactContactApp.swift
+//  ContactContact
+//
+//  Created by Richard Adem on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
